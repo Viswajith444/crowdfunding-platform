ae422adc804be6ad4079da818062ccc17aaccfb5
Viswajith444 <viswajith108@gmail.com>
2025-02-26 23:03:46 +0530

    Fixed Navigation Bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 export default function Home() {
-  return (
-    <div className="grid lg:grid-cols-6 md:grid-cols-4 gap-2 *:aspect-square drop-shadow-lg drop-shadow-black mx-[5rem]">
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-      <div className="bg-amber-200">hi</div>
-    </div>
-  );
+    return (
+        <div className="mx-[5rem] grid gap-8 drop-shadow-lg *:aspect-5/4 *:rounded-md sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+            <div className="bg-amber-200">cards</div>
+        </div>
+    );
 }
